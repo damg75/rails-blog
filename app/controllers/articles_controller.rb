eec1a70cc99bf04749d52022020fb0b25eecf3c0
Daniel Moran <damg75cd@gmail.com>
2021-03-04 13:35:43 -0400

    show, edit, destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     def index
         @articles = Article.all
     end
@@ -8,7 +9,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(title: params[:article][:title])
+        @article = Article.new(article_params)
         if @article.save
             redirect_to articles_path, notice: "Categoría creada"
         else
@@ -17,6 +18,32 @@ class ArticlesController < ApplicationController
     end
 
     def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @article.update(article_params)
+          redirect_to article_path(@article), notice: "Artículo actualizado"
+        else
+          render :edit
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path, alert: "Artículo eliminado"
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
+
+    def set_article
         @article = Article.find(params[:id])
     end
+
 end
