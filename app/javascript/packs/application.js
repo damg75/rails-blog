d75b0a5a1cba145541a80b6e734bbccff8dfbb90
Daniel Moran <damg75cd@gmail.com>
2021-03-04 11:01:48 -0400

    integrar bs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "css/styles"
+// bin/webpack-dev-server para testear la compilacion con webpack, en este caso se agrego bootstrap 4, popper.js y jquery bootstrap 5 ya no maneja esas dependencias
 
 Rails.start()
 Turbolinks.start()
