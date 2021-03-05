c6b0a09542817d23f83a39177109734844044f48
Daniel Moran <damg75cd@gmail.com>
2021-03-04 22:54:11 -0400

    we are done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, only: %i[ new edit update destroy ]
+  before_action :authenticate_user!, only: %i[ new edit create update destroy ]
 
 
   # GET /categories or /categories.json
